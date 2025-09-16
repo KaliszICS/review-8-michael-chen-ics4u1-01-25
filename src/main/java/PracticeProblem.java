004a65eda782ab7a2888564fb60a4f757ba12b8b
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-09-16 14:35:50 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,8 +4,36 @@ public class PracticeProblem {
 
 	}
 
-	public static int min(int num1, int num2, int num3) {
-		return num1;
+	public static String pluralize(String a) {
+		a = a.toLowerCase();
+		if (a.endsWith("ey")) {
+			return "eys";
+		} else if (a.endsWith("y")) {
+			return "ies";
+		} else if (a.endsWith("ife")) {
+			return "ives";
+		} else {
+			return "s";
+		}
 	}
 
+	public static int min(int num1, int num2, int num3) {
+		return (Math.min(num2, num3) < num1 ? Math.min(num2, num3) : num1);
+	}
+
+	public static boolean isLeapYear(int year) {
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
 }
